37991083cb359b8c0c1e72fcceab820a337ba4d7
shayaliza <magicalmind01@gmail.comgit statuss>
2024-07-04 12:22:29 +0530

    opp

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-# import os
-
-# def create_new_folder(folder_name):
-#     try:
-#         os.mkdir(folder_name)
-#         print(f"Folder '{folder_name}' created successfully.")
-#     except FileExistsError:
-#         print(f"Folder '{folder_name}' already exists.")
-
-# if __name__ == "__main__":
-#     create_new_folder("test")
-
-
 import os
 
 def create_new_folder(base_folder):
