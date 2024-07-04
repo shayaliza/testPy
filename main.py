69dfc5f89199235255230fe51c5a06ab3ff1032a
shayaliza <magicalmind01@gmail.comgit statuss>
2024-07-04 11:45:40 +0530

    ok

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
 
-def create_new_folder(folder_name):
+def create_new_folder(test):
     try:
-        os.mkdir(folder_name)
-        print(f"Folder '{folder_name}' created successfully.")
+        os.mkdir(test)
+        print(f"Folder '{test}' created successfully.")
     except FileExistsError:
-        print(f"Folder '{folder_name}' already exists.")
+        print(f"Folder '{test}' already exists.")
 
 if __name__ == "__main__":
-    folder_name = input("Enter folder name: ")
-    create_new_folder(folder_name)
+    # folder_name = input("Enter folder name: ")
+    create_new_folder("test")
